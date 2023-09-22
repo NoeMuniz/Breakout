e4fcc61b2b46749d3d3ddfad74c2a7ca138eaf0c
NoeMuniz <33363753+NoeMuniz@users.noreply.github.com>
2023-09-22 15:41:54 -0500

    added paddle and walls

---
+++ BreakoutRedux/Assets/paddle_movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle_movement : MonoBehaviour
+{
+    public float paddle_speed;
+    public Rigidbody2D rb;
+    private Vector2 moveDir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //processing input
+        ProcessInputs();
+    }
+    void FixedUpdate()
+    {
+        //physics calucations
+        Move();
+    }
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+
+        moveDir = new Vector2(moveX, 0).normalized;
+    }
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * paddle_speed, 0);
+    }
+}
